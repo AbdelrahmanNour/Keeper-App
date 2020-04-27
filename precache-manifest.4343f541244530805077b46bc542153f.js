a875ee371c59527a0f61ecd0d9476638b4acadf0
Abdelrahman Nour <nourdaher015@gmail.com>
2020-04-27 23:43:37 +0200

    Updates

---
+++ precache-manifest.4343f541244530805077b46bc542153f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ab297eda1dcebd4ed4ba02b72452a2",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "4d98b71adbeece867ecf",
+    "url": "/Keeper-App/static/js/2.abb8e6a2.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/Keeper-App/static/js/2.abb8e6a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0ab6eaeced75e4612ea",
+    "url": "/Keeper-App/static/js/main.76652691.chunk.js"
+  },
+  {
+    "revision": "08d80ba59f3cdcbc730e",
+    "url": "/Keeper-App/static/js/runtime-main.c3405e47.js"
+  }
+]);
